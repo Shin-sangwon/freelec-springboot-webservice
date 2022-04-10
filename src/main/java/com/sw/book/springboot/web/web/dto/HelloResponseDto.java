a853148be209f0f5cad0365edf76e52949d56af6
Shin-sangwon <dnjsml10@gmail.com>
2022-04-10 12:43:23 +0900

    spring boot project

---
+++ src/main/java/com/sw/book/springboot/web/web/dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.sw.book.springboot.web.web.dto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
