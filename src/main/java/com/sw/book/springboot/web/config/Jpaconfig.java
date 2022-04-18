c9395edf23e31f98a77fc7c44a92b6ef1bdac9b4
Shin-sangwon <dnjsml10@gmail.com>
2022-04-18 16:53:36 +0900

    spring boot project

---
+++ src/main/java/com/sw/book/springboot/web/config/Jpaconfig.java
@@ -0,0 +1,8 @@
+package com.sw.book.springboot.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class Jpaconfig {}
